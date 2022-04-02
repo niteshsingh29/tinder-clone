0a656ad059e6f6ce4ee1fc62c2d7bf926d780f0f
niteshsingh29 <niteshsingh29072000a@gmail.com>
2022-04-03 00:49:51 +0530

    created api to send data into mongodb

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,22 @@ const mongoose = require("mongoose");
 const Tinder = require("./mongoose_connection/mongoose");
 const port = process.env.PORT || 8000;
 
-
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   res.status(200).send("sending from backend");
+});
+
+app.post("/tinder-api", async (req, res, next) => {
+  try {
+    const result = req.body;
+    const document = new Tinder(result)
+    const sendData = await Tinder.create(document)
+    res.status(201).send(result)
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 app.listen(port, () => {
   console.log("listening on port " + port);
